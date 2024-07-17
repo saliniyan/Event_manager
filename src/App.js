069ebc73b4602568f381966276c032f64f64383b
saliniyan <saliniyanp02@gmail.com>
2024-07-17 10:42:48 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import { useState } from "react";
 import Admin from "./Admin";
 import Student from "./Student";
+import Home from "./Home"
+import { BrowserRouter as Router ,Route, Routes ,Link } from "react-router-dom";
+
 const App = () => {
     const [event,setevent]=useState
-    ([
+    ([  
     {
     id:0,
     name:'hackathon',
@@ -19,15 +22,31 @@ const App = () => {
     }
     ])
 
-    const [admin,setadmin]=useState(false)
-
     return(
+        <Router>
         <div>
-        <button onClick={()=>setadmin(!admin)}>Admin</button>
-        {admin && <Admin event={event} setevent={setevent}/>}
-        <hr />
-        <Student event={event}/>
-        </div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/Admin">Admin</Link>
+                    </li>
+                    <li>
+                        <Link to="/Student">Student</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Admin" element={<Admin event={event} setevent={setevent} />} 
+                />
+                <Route path="/Student" element={<Student event={event} />}
+                />
+            </Routes>
+        </div>  
+        </Router>
     )
 };
 
