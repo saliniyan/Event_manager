8105f7f8162cae812ec02c4860fdc65ab8260b92
saliniyan <saliniyanp02@gmail.com>
2024-08-07 22:34:22 +0530

    improved ui

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,9 +4,12 @@ import './Home.css'
 const Home=()=>
 {
     return(
+        <div>
+            <br />
         <div className="home-container">
         <p>Welcome to home page</p>
         <p>You and create and apply for various events</p>
+        </div>
         </div>
     )
 }
