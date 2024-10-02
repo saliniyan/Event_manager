bffb7c0e6d4285387ac6047859d470684240c14e
saliniyan <saliniyanp02@gmail.com>
2024-10-02 08:49:19 +0530

    added login

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ import Student from "./Student";
 import Home from "./Home"
 import { BrowserRouter as Router ,Route, Routes ,Link } from "react-router-dom";
 import FormPage from "./FormPage";
-import './App.css'
+import './App.css';
+import Login from "./Login";
 
 const App = () => {
     const [event, setevent] = useState(() => {
@@ -17,25 +18,23 @@ const App = () => {
         localStorage.setItem('Events', JSON.stringify(event));
       }, [event]);
       
-    return(
+      return (
         <Router>
-        <div>
-            <nav className="nav">
-                <Link to="/">Home</Link>
-                <Link to="/Admin">Admin</Link>
-                <Link to="/Student">Student</Link>
-            </nav>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/Admin" element={<Admin event={event} setevent={setevent} />} 
-                />
-                <Route path="/Student" element={<Student event={event} />}
-                />
-                <Route path="/form" element={<FormPage />} />
-            </Routes>
-        </div>  
+            <div>
+                <nav className="nav">
+                    <Link to="/">Home</Link>
+                    <Link to="/login">Login</Link>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/Admin" element={<Admin event={event} setevent={setevent} />} />
+                    <Route path="/Student" element={<Student event={event} />} />
+                    <Route path="/form" element={<FormPage />} />
+                </Routes>
+            </div>
         </Router>
-    )
+    );
 };
 
 export default App;
--- client/src/Home.js
+++ client/src/Home.js
@@ -5,8 +5,16 @@ const Home=()=>
 {
     return(
         <div className="home">
-        <div className="home-container">
-        <p>Introducing my Event manager App</p>
+        <p className="heading">Introducing my Event <br />
+        <div class ="sec">
+            Manager App
+        </div></p>
+        <div className="banner">
+        <div className="content">
+            <p>CREATE AND MANAGE COLLEGE EVENTS WITH EASE,<br />
+            STUDENTS CAN APPLY AND PARTICIPATE IN A VARIETY OF EXCITING ACTIVITIES.<br />
+            JOIN THE EXCITEMENT AT YOUR CAMPUS EVENTS!</p>
+        </div>
         </div>
         </div>
     )
