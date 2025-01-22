4523b223d14770c7473391e52b6251ecc2828343
saliniyan <saliniyanp02@gmail.com>
2025-01-22 05:47:51 +0530

    modified ui of home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,22 +1,26 @@
-import React from "react"
-import './Home.css'
+import React from "react";
+import './Home.css';
 
-const Home=()=>
-{
-    return(
-        <div className="home">
-        <p className="heading">Introducing my Event <br />
-        <div class ="sec">
-            Manager App
-        </div></p>
-        <div className="banner">
-        <div className="content">
-            <p>CREATE AND MANAGE COLLEGE EVENTS WITH EASE,<br />
-            STUDENTS CAN APPLY AND PARTICIPATE IN A VARIETY OF EXCITING ACTIVITIES.<br />
-            JOIN THE EXCITEMENT AT YOUR CAMPUS EVENTS!</p>
+const Home = () => {
+    return (
+        <div className="home-container">
+            <header className="home-header">
+                <h1 className="main-heading">Welcome to Event Manager</h1>
+                <p className="sub-heading">
+                    Your ultimate solution to create, manage, and participate in college events!
+                </p>
+            </header>
+            <div className="banner">
+                <div className="banner-content">
+                    <h2 className="tagline">Organize Events Seamlessly</h2>
+                    <p className="description">
+                        Experience the excitement of campus life like never before.<br />
+                        From event creation to participant management, we’ve got you covered.
+                    </p>
+                </div>
+            </div>
         </div>
-        </div>
-        </div>
-    )
-}
+    );
+};
+
 export default Home;
